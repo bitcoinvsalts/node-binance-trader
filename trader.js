6367df53c0f394711e28addf35a12200eb9df258
Pavol Duroska <plasto13@gmail.com>
2020-04-02 10:24:27 +0200

    Add Telebot. Telegram functionality

--- trader.js
+++ trader.js
@@ -8,6 +8,7 @@ const axios = require('axios')
 const Binance = require('node-binance-api')
 const binance = require('binance-api-node').default
 const nodemailer = require('nodemailer')
+const TeleBot = require('telebot')
 
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
@@ -26,8 +27,43 @@ app.listen(process.env.PORT || 8003, () => console.log('NBT auto trader running.
 
 
 //////////////////////////////////////////////////////////////////////////////////
+//              TELEGRAM BOT 
+//////////////////////////////////////////////////////////////////////////////////
 
-const send_email = true
+const telegramToken = 'replace_with_your_BOT_token' //BOT TOKEN -> ask BotFather
+let telChanel = -123456789 //Replace with your Chanel ID. if needed help go uncoment LINES 723 and 724
+
+const telBot = new TeleBot({
+token: telegramToken, // Required. Telegram Bot API token.
+polling: { // Optional. Use polling.
+interval: 700, // Optional. How often check updates (in ms).
+timeout: 0, // Optional. Update polling timeout (0 - short polling).
+limit: 100, // Optional. Limits the number of updates to be retrieved.
+retryTimeout: 5000, // Optional. Reconnecting timeout (in ms).
+// proxy: 'http://username:password@yourproxy.com:8080' // Optional. An HTTP proxy to be used.
+},
+// webhook: { // Optional. Use webhook instead of polling.
+//     key: 'key.pem', // Optional. Private key for server.
+//     cert: 'cert.pem', // Optional. Public key.
+//     url: 'https://....', // HTTPS url to send updates to.
+//     host: '0.0.0.0', // Webhook server host.
+//     port: 443, // Server port.
+//     maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+// },
+allowedUpdates: [], // Optional. List the types of updates you want your bot to receive. Specify an empty list to receive all updates.
+usePlugins: ['askUser'], // Optional. Use user plugins from pluginFolder.
+pluginFolder: '../plugins/', // Optional. Plugin folder location.
+pluginConfig: { // Optional. Plugin configuration.
+// myPluginName: {
+//   data: 'my custom value'
+// }
+}
+});
+//////////////////////////////////////////////////////////////////////////////////
+
+const enable_margin = false //ENABLE OR DISABLE MARGIN TRADING
+const send_email = false // USE SEND MAIL ---- true = YES; false = NO
+const use_telegram = false //USE TELEGRAM 
 const gmail_address = ''
 const gmail_app_password = ''
 const gmailEmail = encodeURIComponent(gmail_address)
@@ -59,7 +95,6 @@ const bnb_client = new Binance().options({
     APISECRET: ''
 })
 
-////////
 
 const binance_client = binance({
     apiKey: '',
@@ -68,7 +103,7 @@ const binance_client = binance({
 
 //////////////////////////////////////////////////////////////////////////////////
 
-const nbt_vers = "0.2.2"
+const nbt_vers = "0.2.3"
 const socket = io('https://nbt-hub.herokuapp.com', { query: "v="+nbt_vers+"&type=client&key=" + bva_key })
 
 socket.on('connect', () => {
@@ -105,6 +140,12 @@ socket.on('buy_signal', async (signal) => {
                         }).catch(error => { console.error('There was an error while sending the email: stop trying') })
                     }, 2000 )
                 })
+            }
+            //SEND TELEGRAM MSG
+            if (use_telegram) {
+                let msg = "BUY_SIGNAL :: ENTER LONG TRADE :: " + signal.stratname + ' ' + signal.pair + ' ' + signal.price+"\n"
+                msg += (signal.score?"score: "+signal.score:'score: NA') + "\n"
+                telBot.sendMessage(telChanel, msg)
             }
             //////
             trading_pairs[signal.pair+signal.stratid] = true
@@ -152,7 +193,7 @@ socket.on('buy_signal', async (signal) => {
                     socket.emit("traded_buy_signal", traded_buy_signal)
                     ////
                     if (user_payload[tresult].trading_type === "real") {
-                        if (margin_pairs.includes(alt+"BTC")) {
+                        if (margin_pairs.includes(alt+"BTC") && enable_margin === true) {
                             bnb_client.mgMarketBuy(alt+"BTC", Number(qty), (error, response) => {
                                 if ( error ) { console.log("ERROR 3355333", error.body) }
                                 else console.log("SUCCESS 222444222")
@@ -195,6 +236,12 @@ socket.on('buy_signal', async (signal) => {
                         }).catch(error => { console.error('There was an error while sending the email: stop trying') })
                     }, 2000 )
                 })
+            }
+            //SEND TELEGRAM MSG
+            if (use_telegram) {
+                let msg = "BUY_SIGNAL :: BUY TO COVER SHORT TRADE :: " + signal.stratname + ' ' + signal.pair + ' ' + signal.price+"\n"
+                msg += (signal.score?"score: "+signal.score:'score: NA') + "\n"
+                telBot.sendMessage(telChanel, msg)
             }
             //////
             console.log(signal.pair, ' ---> BUY', Number(trading_qty[signal.pair+signal.stratid]))
@@ -296,6 +343,12 @@ socket.on('sell_signal', async (signal) => {
                     }, 2000 )
                 })
             }
+            //SEND TELEGRAM MSG
+            if (use_telegram) {
+                let msg = "SELL_SIGNAL :: ENTER SHORT TRADE :: " + signal.stratname + ' ' + signal.pair + ' ' + signal.price+"\n"
+                msg += (signal.score?"score: "+signal.score:'score: NA') + "\n"
+                telBot.sendMessage(telChanel, msg)
+            }
             //////
             trading_pairs[signal.pair+signal.stratid] = true
             trading_types[signal.pair+signal.stratid] = "SHORT"
@@ -386,6 +439,13 @@ socket.on('sell_signal', async (signal) => {
                     }, 2000 )
                 })
             }
+            //SEND TELEGRAM MSG
+            if (use_telegram) {
+                let msg = "SELL_SIGNAL :: SELL TO EXIT LONG TRADE :: " + signal.stratname + ' ' + signal.pair + ' ' + signal.price+"\n"
+                msg += (signal.score?"score: "+signal.score:'score: NA') + "\n"
+                telBot.sendMessage(telChanel, msg)
+            }
+            
             //////
             console.log(signal.pair, ' ---> SELL', Number(trading_qty[signal.pair+signal.stratid]))
             if (signal.pair == 'BTCUSDT') {
@@ -420,7 +480,7 @@ socket.on('sell_signal', async (signal) => {
                     socket.emit("traded_sell_signal", traded_sell_signal)
                     ///
                     if (user_payload[tresult].trading_type === "real") {
-                        if (margin_pairs.includes(alt+"BTC")) {
+                        if (margin_pairs.includes(alt+"BTC") && enable_margin === true) {
                             console.log("QTY =======mgMarketSell======> " + qty + " - " + alt + "BTC")
                             bnb_client.mgMarketSell(alt+"BTC", Number(qty), (error, response) => {
                                 if (error) { console.log("ERROR 722211117", alt, Number(qty), JSON.stringify(error)) }
@@ -589,7 +649,7 @@ socket.on('stop_traded_signal', async (signal) => {
 
 socket.on('user_payload', async (data) => {
     console.log(colors.grey('NBT HUB => user strategies + trading setup updated'))
-    console.log(data.length)
+    // console.log(data.length)
     user_payload = data
 })
 
@@ -657,3 +717,16 @@ async function run() {
 run()
 
 //////////////////////////////////////////////////////////////////////////////////
+//                      TELEGRAM BOT
+/////////////////////////////////////////////////////////////////////////////////
+
+// GET CHANEL ID
+
+telBot.on('/info', async (msg) => {       
+    let response = "Open Trades: "+ _.values(trading_pairs).length+"\n" 
+    // response += "Chanel ID : "+msg.chat.id+"\n"  //IF UNCOMENT SHOW CHANEL ID 
+    // telChanel = msg.chat.id
+    return telBot.sendMessage(telChanel, response)
+});
+
+telBot.start();
