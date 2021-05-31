a5d2d83f17e34233fed0e063a1eedb24bb191e0c
Patrick Banks <Patrick.Banks@wolterskluwer.com>
2021-05-31 19:28:59 +1000

    Web server info endpoints.

--- src/trader/http.ts
+++ src/trader/http.ts
@@ -1,15 +1,36 @@
 import * as http from "http"
 import express from "express"
 
-import logger from "../logger"
+import logger, { loggerOutput } from "../logger"
 import env from "./env"
+import { tradingMetaData, virtualBalances } from "./trader"
 
 export default function startWebserver(): http.Server {
     const webserver = express()
     webserver.get("/", (req, res) =>
         res.send("Node Binance Trader is running.")
     )
+    // Allow user to see open trades
+    webserver.get("/trades", (req, res) =>
+        res.send(HTMLFormat(tradingMetaData.tradesOpen))
+    )
+    // Allow user to see configured strategies
+    webserver.get("/strategies", (req, res) =>
+        res.send(HTMLFormat(tradingMetaData.strategies))
+    )
+    // Allow user to see virtual balances
+    webserver.get("/virtual", (req, res) =>
+        res.send(HTMLFormat(virtualBalances))
+    )
+    // Allow user to see log
+    webserver.get("/log", (req, res) => 
+        res.send(HTMLFormat(loggerOutput.slice().reverse().join("\r\n")))
+    )
     return webserver.listen(env().TRADER_PORT, () =>
         logger.info(`Webserver started on port ${env().TRADER_PORT}.`)
     )
+}
+
+function HTMLFormat(data: any): string {
+    return "<html><body><pre><code>" + (typeof data == "string" ? data : JSON.stringify(data, null, 4)) + "</code></pre></body></html>"
 }
